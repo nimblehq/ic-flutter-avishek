6ac376edc012f0f1f85a9a791e07841a1066b991
Avishek Khan <avishek@nimblehq.co>
2023-08-09 19:01:10 +0700

    [53] [Integrate] Survey details screen

--- lib/ui/surveydetail/survey_detail_screen.dart
+++ lib/ui/surveydetail/survey_detail_screen.dart
@@ -1,18 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_survey/ui/surveydetail/survey_detail_state.dart';
+import 'package:flutter_survey/ui/surveydetail/survey_detail_view_model.dart';
 import 'package:flutter_survey/ui/surveydetail/survey_intro.dart';
 import 'package:go_router/go_router.dart';
 
+import '../../di/di.dart';
+import '../../usecases/get_survey_detail_use_case.dart';
 import '../home/survey_ui_model.dart';
 import '../widget/dimmed_image_background.dart';
+import '../widget/loading_indicator.dart';
 
 const Duration _pageScrollDuration = Duration(milliseconds: 200);
 const double _initialBackgroundScale = 1;
 const double _finalBackgroundScale = 1.5;
 const _imageScaleAnimationDurationInMillis = 700;
 
-final shouldAnimateBackgroundScaleProvider =
+final shouldZoomInBackgroundProvider =
     StateProvider.autoDispose<bool>((_) => false);
+
+final surveyDetailViewModelProvider =
+    StateNotifierProvider.autoDispose<SurveyDetailViewModel, SurveyDetailState>(
+        (ref) {
+  return SurveyDetailViewModel(
+    getIt.get<GetSurveyDetailUseCase>(),
+  );
+});
 
 class SurveyDetailScreenKey {
   SurveyDetailScreenKey._();
@@ -39,11 +53,12 @@ class SurveyDetailScreenState extends ConsumerState<SurveyDetailScreen> {
   @override
   void initState() {
     super.initState();
-    Future.delayed(Duration.zero, () {
-      final shouldAnimateBackgroundScale =
-          ref.watch(shouldAnimateBackgroundScaleProvider.notifier);
-      shouldAnimateBackgroundScale.state = !shouldAnimateBackgroundScale.state;
-    });
+    final surveyUiModel = widget.surveyUiModel;
+    if (surveyUiModel != null) {
+      ref
+          .read(surveyDetailViewModelProvider.notifier)
+          .loadSurveyDetail(surveyUiModel);
+    }
   }
 
   @override
@@ -56,28 +71,45 @@ class SurveyDetailScreenState extends ConsumerState<SurveyDetailScreen> {
   Widget build(BuildContext context) {
     return WillPopScope(
       onWillPop: () async {
-        _animateScaleAndPop();
+        _zoomOutAndPop();
         return false;
       },
       child: Scaffold(
         resizeToAvoidBottomInset: false,
-        body: _buildSurveyScreen(widget.surveyUiModel),
+        body: ref.watch(surveyDetailViewModelProvider).when(
+              init: () => const SizedBox.shrink(),
+              loading: () => _buildSurveyScreen(widget.surveyUiModel, true),
+              success: () => _buildSurveyScreen(widget.surveyUiModel, false),
+              error: (message) {
+                WidgetsBinding.instance.addPostFrameCallback((_) {
+                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                      content: Text(message ??
+                          AppLocalizations.of(context)!.errorGeneric)));
+                });
+                return _buildSurveyScreen(widget.surveyUiModel, false);
+              },
+            ),
       ),
     );
   }
 
-  Widget _buildSurveyScreen(SurveyUiModel? survey) {
+  Widget _buildSurveyScreen(SurveyUiModel? survey, bool isLoading) {
+    Future.delayed(Duration.zero, () {
+      final shouldZoomInBackground =
+      ref.watch(shouldZoomInBackgroundProvider.notifier);
+      shouldZoomInBackground.state = true;
+    });
     return survey != null
         ? Stack(
             children: [
               Consumer(builder: (_, ref, __) {
-                final shouldAnimateBackgroundScale =
-                    ref.watch(shouldAnimateBackgroundScaleProvider);
+                final shouldZoomInBackground =
+                    ref.watch(shouldZoomInBackgroundProvider);
                 return AnimatedScale(
                   duration: const Duration(
                     milliseconds: _imageScaleAnimationDurationInMillis,
                   ),
-                  scale: shouldAnimateBackgroundScale
+                  scale: shouldZoomInBackground
                       ? _finalBackgroundScale
                       : _initialBackgroundScale,
                   child: DimmedImageBackground(
@@ -86,6 +118,11 @@ class SurveyDetailScreenState extends ConsumerState<SurveyDetailScreen> {
                 );
               }),
               SafeArea(child: _buildSurveyQuestionPager(survey)),
+              Center(
+                child: isLoading
+                    ? const LoadingIndicator()
+                    : const SizedBox.shrink(),
+              )
             ],
           )
         : const SizedBox.shrink();
@@ -97,7 +134,7 @@ class SurveyDetailScreenState extends ConsumerState<SurveyDetailScreen> {
       SurveyIntro(
         survey: survey,
         onNext: () => _gotoNextPage(),
-        onClose: _animateScaleAndPop,
+        onClose: _zoomOutAndPop,
       ),
     );
 
@@ -116,10 +153,10 @@ class SurveyDetailScreenState extends ConsumerState<SurveyDetailScreen> {
     );
   }
 
-  void _animateScaleAndPop() {
-    final shouldAnimateBackgroundScale =
-        ref.watch(shouldAnimateBackgroundScaleProvider.notifier);
-    shouldAnimateBackgroundScale.state = !shouldAnimateBackgroundScale.state;
+  void _zoomOutAndPop() {
+    final shouldZoomInBackground =
+        ref.watch(shouldZoomInBackgroundProvider.notifier);
+    shouldZoomInBackground.state = false;
     Future.delayed(
       const Duration(milliseconds: _imageScaleAnimationDurationInMillis),
       context.pop,
